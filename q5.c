3568f13856f8dabd076926eb8fc00c0656e08700
Thais Menezes <thaismenezes3108@gmail.com>
2025-05-21 14:10:29 -0300

    Update q5.c

--- q5.c
+++ q5.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_NUM 50
 
-int inverterNumero(int num) {
-    int n1, n2, n3, n4;
-    int resultado;
-    if(num <= 9999 && num >= 1000) {
-        n1 = num / 1000;
-        n2 = (num / 100) % 10;
-        n3 = (num / 10) % 10;
-        n4 = num % 10;
-        resultado = (n4*1000)+(n3*100)+(n2*10)+n1;
-        return resultado;
+void inverterString(char *num) {
+    int inicio = 0;
+    int fim = strlen(num) - 1;
+    char temp;
+
+    while (inicio < fim) {
+        temp = num[inicio];
+        num[inicio] = num[fim];
+        num[fim] = temp;
+
+        inicio++;
+        fim--;
     }
-    if(num <= 999 && num >= 100) {
-        n1 = num / 100;
-        n2 = (num / 10) % 10;
-        n3 = num % 10;
-        resultado = (n3*100) + (n2*10) + n1;
-        return resultado;
-    } 
-    if (num >= 10 && num <= 99) {
-        n1 = num/10; //n1 = 5
-        n2 = num% 10;
-        resultado = (n2*10) + n1;
-        return resultado;
-}
 }
 
-int main() {
-    int num;
+int main(){
+    char num[MAX_NUM];
+    
     printf("Informe um número:\n");
-    scanf("%d", &num);
-    inverterNumero(num);
-    printf("%d", inverterNumero(num));
+    fgets(num, MAX_NUM, stdin);
+    
+    inverterString(num);
+    printf("%s", num);
 }
